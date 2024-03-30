9b1ca0336b094d50c7331a9001325a266683e11c
sriii3 <139474066+sriii3@users.noreply.github.com>
2024-03-30 11:33:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+/*==========typing animation=========*/
+var typed = new Typed(".typing",
+{
+    strings : ["Under Graduate","Web Designer","Web Developer","Java Developer","YouTuber"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
